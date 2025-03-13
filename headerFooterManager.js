9898cc5b591c8451d6c63ef7eb22fd4aec3cd496
Keith <Keith@gmail.com>
2025-03-13 13:08:56 +0300

    new0ne

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -1,116 +1,106 @@
 class SpecialHeader extends HTMLElement {
     connectedCallback() {
-                this.innerHTML = `
+        // Dynamic logo and links (can be configured globally)
+        const logoSrc = "img/user/header-logo-LxR0VE.svg";
+        const logoAlt = "Company Logo";
+        const navLinks = [
+            { name: "Home", url: "index.html" },
+            { name: "Markets", url: "markets.html" },
+            { name: "Company", dropdown: [
+                { name: "About", url: "about.html" },
+                { name: "Blog", url: "blog.html" },
+                { name: "Contact", url: "contact.html" }
+            ] },
+            { name: "Resources", dropdown: [
+                { name: "Help Center", url: "help-center.html" },
+                { name: "Customers", url: "customers.html" },
+                { name: "Roadmap", url: "roadmap.html" },
+                { name: "Legal Docs", url: "legal-docs.html", icon: "fas fa-gavel fa-sm" }
+            ] }
+        ];
+
+        this.innerHTML = `
             <header class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">
-                <img src="img/user/header-logo-LxR0VE.svg" alt="logo" width="122" height="36" class="d-inline-block">
-            </a>
-            <div class="collapse navbar-collapse d-flex justify-content-between d-none d-xl-block" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="index.html" id="dropdown-home" data-bs-toggle="dropdown" aria-expanded="false">Home</a>
-                       
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="markets.html">Markets</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-company" data-bs-toggle="dropdown" aria-expanded="false">Company</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="about.html">About</a></li>
-                            <li><a class="dropdown-item" href="blog.html">Blog</a></li>
-                            <li><a class="dropdown-item" href="contact.html">Contact</a></li>
-                        </ul>
-                    </li>
-                   
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-resources" data-bs-toggle="dropdown" aria-expanded="false">Resources</a>
-                        <ul class="dropdown-menu dropdown-large-menu">
-                            <li>
-                                <div class="row">
-                                    <div class="col-6">
-                                        <ul class="list-unstyled">
-                                            <li><a class="dropdown-item" href="help-center.html">Help Center</a></li>
-                                            <li><a class="dropdown-item" href="customers.html">Customers</a></li>
-                                            <li><a class="dropdown-item" href="roadmap.html">Roadmap</a></li>
-                                            <li><a class="dropdown-item" href="legal-docs.html">Legal Docs<i class="fas fa-gavel fa-sm"></i></a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-6 border-start child-menu-text">
-                                        <p>Adipiscing elit sed do eiusmod incididunt ut labore dolore magna lorem ipsum sit dolor amet consectetur</p>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                <div class="optional-link d-flex align-items-center ms-4 d-none d-xl-block">
-                    <a href="signin.html" class="btn btn-link me-3"><i class="fas fa-circle-user"></i>Login</a>
-                    <a href="signin.html" class="btn btn-info">Request a demo</a>
+                <div class="container">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="${logoSrc}" alt="${logoAlt}" width="122" height="36" class="d-inline-block">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">${this.generateNavItems(navLinks)}</ul>
+                        <div class="optional-link d-flex align-items-center ms-4">
+                            <a href="signin.html" class="btn btn-link me-3" aria-label="Login"><i class="fas fa-circle-user"></i> Login</a>
+                            <a href="signin.html" class="btn btn-info">Request a demo</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </header>
-      `;
+            </header>
+        `;
     }
- }
+
+    generateNavItems(navLinks) {
+        return navLinks.map(link => {
+            if (link.dropdown) {
+                return `
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">${link.name}</a>
+                        <ul class="dropdown-menu">
+                            ${link.dropdown.map(sub => `<li><a class="dropdown-item" href="${sub.url}">${sub.name} ${sub.icon ? `<i class='${sub.icon}'></i>` : ""}</a></li>`).join('')}
+                        </ul>
+                    </li>
+                `;
+            } else {
+                return `<li class="nav-item"><a class="nav-link" href="${link.url}">${link.name}</a></li>`;
+            }
+        }).join('');
+    }
+}
 
 class SpecialFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer class="py-5 in-cirro-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="row gx-0 mb-3">
-                        <div class="col-md-12 col-lg-3 footer-logo">
-                            <img src="img/user/footer-logo-5vqlhb.svg" alt="footer-logo" width="122" height="36" class="mb-1 d-block">
-                            <a class="lead footer-email" href="mailto:info@cirro-invest.id">info@cirro-invest.id</a>
-                        </div>
-                        <div class="col-md-12 col-lg-9">
-                            <div class="d-flex flex-column flex-xl-row justify-content-xl-end">
-                                <ul class="list-inline in-footer-link order-last order-xl-first">
-                                    <li class="list-inline-item"><a href="#">Contact</a></li>
-                                    <li class="list-inline-item"><a href="#">FAQ</a></li>
-                                    <li class="list-inline-item"><a href="#">Carreers</a></li>
-                                    <li class="list-inline-item"><a href="#">Partners</a></li>
-                                    <li class="list-inline-item"><a href="#">Integrations</a></li>
-                                    <li class="list-inline-item"><a href="#">Security</a></li>
-                                </ul>
-                                <!-- social media begin -->
-                                <div class="social-media-list small hstack gap-3 ms-lg-5 order-first order-xl-last">
-                                <div><a href="https://www.facebook.com/indonez" class="color-facebook text-decoration-none"><i class="fab fa-facebook-square"></i> Facebook</a></div>
-                                <div><a href="https://twitter.com/indonez_tw" class="color-twitter text-decoration-none"><i class="fab fa-twitter"></i> Twitter</a></div>
-                                <div><a href="#some-link" class="color-linkedin text-decoration-none"><i class="fab fa-linkedin"></i> Linkedin</a></div>
-                                </div>
-                                <!-- social media end -->
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-3 mt-5 d-flex">
-                            <div class="align-self-end">
-                                <p class="mb-0 copyright-text">©2022 Cirro Invest Ltd. All Rights Reserved.</p>
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-9 d-lg-flex justify-content-lg-end d-none d-lg-block">
-                            <div class="align-self-end">
-                                <nav class="nav in-footer-link-2">
-                                    <a class="nav-link border-end-md" href="#">Risk Disclosure</a>
-                                    <a class="nav-link border-end-md" href="#">Privacy policy</a>
-                                    <a class="nav-link border-end-md" href="#">Return policy</a>
-                                    <a class="nav-link border-end-md" href="#">Customer Agreement</a>
-                                    <a class="nav-link pe-0" href="#">AML policy</a>
-                                </nav>
-                            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 footer-logo">
+                        <img src="img/user/footer-logo.svg" alt="Company Footer Logo" width="122" height="36" class="mb-1 d-block">
+                        <a class="lead footer-email" href="mailto:info@example.com">info@example.com</a>
+                    </div>
+                    <div class="col-lg-9 d-flex justify-content-lg-end">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><a href="#">Contact</a></li>
+                            <li class="list-inline-item"><a href="#">FAQ</a></li>
+                            <li class="list-inline-item"><a href="#">Careers</a></li>
+                            <li class="list-inline-item"><a href="#">Partners</a></li>
+                            <li class="list-inline-item"><a href="#">Integrations</a></li>
+                            <li class="list-inline-item"><a href="#">Security</a></li>
+                        </ul>
+                        <div class="social-media-list small hstack gap-3 ms-lg-5">
+                            <a href="#" class="text-decoration-none"><i class="fab fa-facebook-square"></i> Facebook</a>
+                            <a href="#" class="text-decoration-none"><i class="fab fa-twitter"></i> Twitter</a>
+                            <a href="#" class="text-decoration-none"><i class="fab fa-linkedin"></i> Linkedin</a>
                         </div>
                     </div>
                 </div>
+                <div class="row mt-4">
+                    <div class="col-lg-12 text-center">
+                        <p class="mb-0 copyright-text">© ${new Date().getFullYear()} Your Company. All Rights Reserved.</p>
+                        <nav class="nav justify-content-center mt-2">
+                            <a class="nav-link" href="#">Risk Disclosure</a>
+                            <a class="nav-link" href="#">Privacy Policy</a>
+                            <a class="nav-link" href="#">Return Policy</a>
+                            <a class="nav-link" href="#">Customer Agreement</a>
+                            <a class="nav-link" href="#">AML Policy</a>
+                        </nav>
+                    </div>
+                </div>
             </div>
-        </div>
-    </footer>`;
+        </footer>
+        `;
     }
 }
-
 
 customElements.define('special-header', SpecialHeader);
 customElements.define('special-footer', SpecialFooter);
